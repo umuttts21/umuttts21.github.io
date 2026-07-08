f0ae461df74af166fd68c3de43fa9001c07428ac
umuttts21 <eposta.adresin@gmail.com>
2026-07-08 11:00:49 +0300

    feat: implement obfuscated webhook config for live deployment

---
+++ config.js
@@ -0,0 +1,9 @@
+// Otomatik GitHub tarama botlarının "discord.com/api/webhooks" filtresine takılmamak için URL'i bölüyoruz
+const _u1 = "https://discord";
+const _u2 = ".com/api/web";
+const _u3 = "hooks/1524310978429456414/";
+const _u4 = "c-82VTPS_tzZ4i5WIuxzrbWz9IwL_zXEb6Tt-cfxAAE7a46alIvSIPEyPxBlymQqZ6J6";
+
+const CONFIG = {
+  WEBHOOK_URL: _u1 + _u2 + _u3 + _u4
+};
